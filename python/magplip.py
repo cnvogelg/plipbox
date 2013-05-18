e2632a8a685f80dac5944802e502ab77cab049ed
Christian Vogelgsang <C.Vogelgsang@web.de>
2013-05-18 19:58:27 +0200

    better magplip exception handling

--- python/magplip.py
+++ python/magplip.py
@@ -23,7 +23,7 @@ class MagPlip:
         # clear HS_LINE
         self.vpar.clr_control_mask(vpar.BUSY_MASK)
     
-    def wait_select(self, value, timeout=1):
+    def wait_select(self, value, timeout=1, ctx="", start=0):
         """wait for SELECT signal"""
         t = time.time()
         end = t + timeout
@@ -40,9 +40,11 @@ class MagPlip:
             rem = end - t
             self.vpar.poll_state(rem)
             t = time.time()
-        return found
+        if not found:
+          delta = t - start
+          raise MagPlipError("%s: no select. delta=%5.3f timeout=%d" % (ctx, delta, timeout))
     
-    def wait_line_toggle(self, expect, timeout=1):
+    def wait_line_toggle(self, expect, timeout=1, ctx="", start=0):
         """wait for toggle on POUT signal"""
         #print expect
         t = time.time()
@@ -59,12 +61,15 @@ class MagPlip:
             
             # check for SELECT -> arbitration loss?
             if (s & vpar.SEL_MASK) != vpar.SEL_MASK:
-                return False
+                delta = t - start
+                raise MagPlipError("%s: lost select in line toggle. delta=%5.3f timeout=%d" % (ctx, delta, timeout))
                 
             rem = end - t
             self.vpar.poll_state(rem)
             t = time.time()
-        return found
+        if not found:
+          delta = t - start
+          raise MagPlipError("%s: missing line toggle. delta=%5.3f timeout=%d" % (ctx, delta, timeout))
     
     def toggle_busy(self, value):
         if value:
@@ -72,39 +77,28 @@ class MagPlip:
         else:
             self.vpar.set_control_mask(vpar.BUSY_MASK)
             
-    def set_next_byte(self, toggle_expect, value, timeout=1):
-        ok = self.wait_line_toggle(toggle_expect, timeout)
-        if not ok:
-            return False
+    def set_next_byte(self, toggle_expect, value, timeout=1, ctx="", start=0):
+        self.wait_line_toggle(toggle_expect, timeout, ctx, start)
         self.vpar.set_data(value)
         self.toggle_busy(toggle_expect)
         return True
 
-    def set_next_word(self, toggle_expect, value, timeout=1):
+    def set_next_word(self, toggle_expect, value, timeout=1, ctx="", start=0):
         v1 = value / 256
-        ok = self.set_next_byte(toggle_expect, v1, timeout)
-        if not ok:
-            return False
+        self.set_next_byte(toggle_expect, v1, timeout, ctx + "/b1", start)
         v2 = value % 256
-        ok = self.set_next_byte(not toggle_expect, v2, timeout)
-        return ok
+        self.set_next_byte(not toggle_expect, v2, timeout, ctx + "/b2", start)
 
-    def get_next_byte(self, toggle_expect, timeout=1):
-        ok = self.wait_line_toggle(toggle_expect, timeout)
-        if not ok:
-            return None
+    def get_next_byte(self, toggle_expect, timeout=1, ctx="", start=0):
+        self.wait_line_toggle(toggle_expect, timeout, ctx, start)
         self.vpar.poll_state(0)
         data = self.vpar.peek_data()
         self.toggle_busy(not toggle_expect)
         return data
     
-    def get_next_word(self, toggle_expect, timeout=1):
-        v1 = self.get_next_byte(toggle_expect, timeout)
-        if v1 == None:
-            return None
-        v2 = self.get_next_byte(not toggle_expect, timeout)
-        if v2 == None:
-            return None
+    def get_next_word(self, toggle_expect, timeout=1, ctx="", start=0):
+        v1 = self.get_next_byte(toggle_expect, timeout, ctx + "/b1", start)
+        v2 = self.get_next_byte(not toggle_expect, timeout, ctx + "/b2", start)
         return v1 * 256 + v2
 
     # ----- public API ----
@@ -120,44 +114,30 @@ class MagPlip:
         self.vpar.trigger_ack()
         try:
             # wait for select
-            ok = self.wait_select(True, timeout)
-            if not ok:
-                raise MagPlipError("tx ERROR: Waiting for select")
+            self.wait_select(True, timeout, "tx", trigger)
             start = time.time()
             first_delta = start - trigger
             # set byte for magic code NO_CRC
-            ok = self.set_next_byte(True, 0x02, timeout)
-            if not ok:
-                raise MagPlipError("tx ERROR: CRC flag")
+            self.set_next_byte(True, 0x02, timeout, "tx:crc flag", start)
             # send size
             size = len(raw_pkt) + 2 # add crc
-            ok = self.set_next_word(False, size, timeout)
-            if not ok:
-                raise MagPlipError("tx ERROR: size value")
+            self.set_next_word(False, size, timeout, "tx:size", start)
             # send CRC
             crc = 0
-            ok = self.set_next_word(False, crc, timeout)
-            if not ok:
-                raise MagPlipError("tx ERROR: CRC value")
+            self.set_next_word(False, crc, timeout, "tx:crc", start)
             # send data
             toggle = False
             pos = 0
             for a in raw_pkt:
-                ok = self.set_next_byte(toggle, ord(a), timeout)
-                if not ok:
-                    raise MagPlipError("tx ERROR: data @%d" % pos)
+                self.set_next_byte(toggle, ord(a), timeout, "tx:data@%d" % pos, start)
                 toggle = not toggle
                 pos += 1
             # wait for final toggle
-            ok = self.wait_line_toggle(toggle, timeout)
-            if not ok:
-                raise MagPlipError("tx ERROR: final toggle")
+            self.wait_line_toggle(toggle, timeout, "tx:final toggle", start)
             end = time.time()
             data_delta = end - start
             # wait for select end
-            ok = self.wait_select(False, timeout)
-            if not ok:
-                raise MagPlipError("tx ERROR: Waiting for unselect")
+            self.wait_select(False, timeout, "tx:end select", start)
             last = time.time()
             last_delta = last - end
             return (first_delta, data_delta, last_delta)
@@ -170,47 +150,33 @@ class MagPlip:
         try:
             first = time.time()
             # first assume SELECT to be set
-            ok = self.wait_select(True, timeout)
-            if not ok:
-              return None,None
+            self.wait_select(True, timeout, "tx", first)
             start = time.time()
             first_delta = start - first
             # get magic byte
-            magic = self.get_next_byte(True, timeout)
-            if not magic:
-                raise MagPlipError("rx ERROR: no magic")
+            magic = self.get_next_byte(True, timeout, "rx:magic byte,", start)
             # get crc mode
-            crc_mode = self.get_next_byte(False, timeout)
-            if not crc_mode:
-                raise MagPlipError("rx ERROR: no crc mode")
+            crc_mode = self.get_next_byte(False, timeout, "rx:crc flag", start)
             if crc_mode < 1 or crc_mode > 2:
-                raise MagPlipError("rx ERROR: invalid CRC mode: %d" % crc_mode)
+                raise MagPlipError("rx:invalid CRC mode: %d" % crc_mode)
             # get size
-            size = self.get_next_word(True, timeout)
-            if size == None:
-                raise MagPlipError("rx ERROR: no size")
+            size = self.get_next_word(True, timeout, "rx:size", start)
             # get crc
-            crc = self.get_next_word(True, timeout)
-            if crc == None:
-                raise MagPlipError("rx ERROR: no crc value")
+            crc = self.get_next_word(True, timeout, "rx:crc", start)
             size -= 2 # skip crc
             # get data
             toggle = True
             pos = 0
             raw_pkt = ""
             for i in xrange(size):
-                d = self.get_next_byte(toggle, timeout)
-                if d == None:
-                    raise MagPlipError("rx ERROR: data @%d" % pos)
+                d = self.get_next_byte(toggle, timeout, "rx:data@%d" % pos, start)
                 raw_pkt += chr(d)
                 toggle = not toggle
                 pos = pos+1
             end = time.time()
             data_delta = end - start
             # wait for select gone
-            ok = self.wait_select(False, timeout)
-            if not ok:
-                raise MagPlipError("rx ERROR: no unselect")
+            self.wait_select(False, timeout, "rx:end select", start)
             last = time.time()
             last_delta = last - end
             return raw_pkt,(first_delta, data_delta, last_delta)
