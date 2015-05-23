47fabf600a20203008edf508483eb40cd7fc0797
Christian Vogelgsang <C.Vogelgsang@web.de>
2015-05-23 20:16:28 +0200

    fixed pcapio for osx

--- python/ethercap/pcapio.py
+++ python/ethercap/pcapio.py
@@ -15,40 +15,66 @@ import os
 import struct
 import threading
 
+import time
+
 # parse options
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
   print("Usage:",sys.argv[0],"<interface>")
   sys.exit(1)
 ifc = sys.argv[1]
 
-# create pcap instance
-p = pcap.pcap(ifc)
+# verbose?
+verbose = True if len(sys.argv) > 2 else False
 
+# create pcap instance
+# Mac OSX needs a hack here: without timeout it would block for ever
+# now packets are reported after the timeout
+if sys.platform == 'darwin':
+  p = pcap.pcap(ifc, timeout_ms=100)
+else:
+  p = pcap.pcap(ifc)
+
+# get stdin/stdout streams
 stdin = sys.stdin.fileno()
 stdout = sys.stdout.fileno()
 
-# read loop
+# callback for incoming pcap packet
+def got_packet(ts, pkt):
+  if verbose:
+    print(time.time(),"GOT",len(pkt))
+  else:
+    hdr = struct.pack("H", len(pkt))
+    os.write(stdout, hdr)
+    os.write(stdout, pkt)
+
+# read loop: get packets via pcap and write them to stdout
 def read_loop():
   try:
-    for ts, pkt in p:
-      hdr = struct.pack("H", len(pkt))
-      os.write(stdout, hdr)
-      os.write(stdout, pkt)
-  except OSError:
+    while True:
+      # dispatch forever
+      p.dispatch(0, got_packet)
+  except Exception:
     pass
 
+# write loop: get packets from stdin and send via pcap
+def write_loop():
+  try:
+    while True:
+      # read from stdin
+      hdr = os.read(stdin, 2)
+      # error reading packet from stdin
+      if hdr is None or len(hdr) == 0:
+        break
+      # read packet from stdin
+      size = struct.unpack("H", hdr)[0]
+      data = os.read(stdin, size)
+      p.inject(data, size)
+  except Exception:
+    pass
+
+# run read loop in own thread
 t = threading.Thread(target=read_loop)
 t.run()
 
-# write loop
-try:
-  while True:
-    # read from stdin
-    hdr = os.read(stdin, 2)
-    if hdr is None or len(hdr) == 0:
-      break
-    size = struct.unpack("H", hdr)[0]
-    data = os.read(stdin, size)
-    p.inject(data, size)
-except OSError:
-  pass
+# write loop runs in main thread
+write_loop()
